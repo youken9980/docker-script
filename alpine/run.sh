4480390522545d46f13411b83bf651ada8a252f0
youken9980 <youken9980@163.com>
2020-10-13 15:31:58 +0800

    修改脚本

--- alpine/run.sh
+++ alpine/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-IMAGE_TAG="youken9980/openssh-server:alpine"
+imageTag="youken9980/openssh-server:alpine"
 
 function dockerRm() {
-    CONTAINER_ID=$(docker ps -aq --filter ancestor="${IMAGE_TAG}")
-    if [ "${CONTAINER_ID}" != "" ]; then
-        docker rm $(docker stop "${CONTAINER_ID}")
+    containerId=$(docker ps -aq --filter ancestor="${imageTag}")
+    if [ "${containerId}" != "" ]; then
+        docker rm $(docker stop "${containerId}")
     fi
 }
 
 dockerRm
-docker run -d -p 22:22 "${IMAGE_TAG}"
-docker exec -it $(docker ps -aq --filter ancestor="${IMAGE_TAG}") /bin/sh
+docker run -d -p 22:22 "${imageTag}"
+docker exec -it $(docker ps -aq --filter ancestor="${imageTag}") /bin/sh
--- centos/run.sh
+++ centos/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-IMAGE_TAG="youken9980/centos:latest"
+imageTag="youken9980/centos:latest"
 
 function dockerRm() {
-    CONTAINER_ID=$(docker ps -aq --filter ancestor="${IMAGE_TAG}")
-    if [ "${CONTAINER_ID}" != "" ]; then
-        docker rm $(docker stop "${CONTAINER_ID}")
+    containerId=$(docker ps -aq --filter ancestor="${imageTag}")
+    if [ "${containerId}" != "" ]; then
+        docker rm $(docker stop "${containerId}")
     fi
 }
 
 dockerRm
-docker run -it --rm "${IMAGE_TAG}"
+docker run -it --rm "${imageTag}"
