b9aed99193ac3927c9a2f31300be79ebdc539567
youken9980 <youken9980@163.com>
2020-11-09 16:21:32 +0800

    更新脚本

--- keepalived-nginx/check_service.sh
+++ keepalived-nginx/check_service.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 cmdCheck="ps aux --no-heading | grep 'nginx' | grep 'master' | grep -v '<defunct>' | wc -l"
-cmdNginx="nginx -c /etc/nginx/nginx.conf"
+cmdStart="nginx -c /etc/nginx/nginx.conf"
 counter="$(eval ${cmdCheck})"
 if [ "${counter}" = "0" ]; then
     echo "Starting nginx..."
-    eval ${cmdNginx}
+    eval ${cmdStart}
     sleep 2
     counter="$(eval ${cmdCheck})"
     if [ "${counter}" = "0" ]; then
