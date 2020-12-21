c66899b12356002d06b27f6b7ec0c57fc5413414
youken9980 <youken9980@163.com>
2020-12-21 14:50:04 +0800

    更新脚本

--- jenkins/master/run.sh
+++ jenkins/master/run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+cleanup="false"
 imageTag="youken9980/jenkins-master:latest"
 containerName="jenkins-master"
 network="mynet"
 dataHome="~/dockerVolume/jenkins/master/data"
+dataHome="$(eval readlink -f ${dataHome})"
 mavenRepository="~/maven-repository"
-gradleRepository="~/gradle-repository"
-
-dataHome="$(eval readlink -m ${dataHome})"
 mavenRepository="$(eval readlink -f ${mavenRepository})"
+gradleRepository="~/gradle-repository"
 gradleRepository="$(eval readlink -f ${gradleRepository})"
-eval "mkdir -p ${dataHome}"
 
 function dockerRm() {
     filter="$1"
@@ -29,19 +28,27 @@ function dockerLogsUntil() {
     endpoint="$2"
     containerId=$(docker ps -aq --filter "${filter}")
     nohup docker logs -f "${containerId}" > "/tmp/${containerId}.log" 2>&1 &
-    sleep 1s
     PID=$(ps aux | grep "docker" | grep ${containerId} | awk '{print $2}' | sort -nr | head -1)
     if [ "${PID}" != "" ]; then
         eval "tail -f --pid=${PID} /tmp/${containerId}.log | sed '/${endpoint}/q'"
-        kill -9 ${PID}
+        kill ${PID}
         rm /tmp/${containerId}.log
     fi
+}
+
+function rebuid() {
+    path=$1
+    if [ -e "${path}" ]; then
+        eval "rm -rf ${path}"
+    fi
+    eval "mkdir -p ${path}"
 }
 
 function dockerRun() {
     # 默认情况下，基于JNLP的Jenkins代理通过TCP端口50000与Jenkins主站进行通信。 -p 50000:50000
     docker run -d -p 8090:8080 \
         -e JAVA_OPTS="-Duser.timezone=Asia/Shanghai -Dfile.encoding=UTF8 -Dsun.jnu.encoding=UTF8" \
+        -v /var/run/docker.sock:/var/run/docker.sock \
         -v ${dataHome}:/var/jenkins_home \
         -v ${mavenRepository}:/app/maven-repository \
         -v ${gradleRepository}:/app/gradle-repository \
@@ -58,10 +65,18 @@ function sedSource() {
 }
 
 dockerRm "ancestor=${imageTag}"
+if [ "${cleanup}" = "true" ]; then
+    rebuid "${dataHome}"
+    dockerRun
+    dockerRm "ancestor=${imageTag}"
+    sedSource
+fi
 dockerRun
-sedSource
-dockerRm "ancestor=${imageTag}"
-dockerRun
+
+# /usr/share/jenkins/jenkins.war
+
+# Jenkins initial setup is required. An admin user has been created and a password generated.
+# This may also be found at: /var/jenkins_home/secrets/initialAdminPassword
 
 # 已安装工具的路径和版本号
 # /opt/java/openjdk/bin/java jdk-8u272
--- jenkins/single/run.sh
+++ jenkins/single/run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
+cleanup="false"
 imageTag="youken9980/jenkins-single:latest"
 containerName="jenkins-single"
 network="mynet"
 dataHome="~/dockerVolume/jenkins/single/data"
+dataHome="$(eval readlink -f ${dataHome})"
 mavenRepository="~/maven-repository"
-gradleRepository="~/gradle-repository"
-
-dataHome="$(eval readlink -m ${dataHome})"
 mavenRepository="$(eval readlink -f ${mavenRepository})"
+gradleRepository="~/gradle-repository"
 gradleRepository="$(eval readlink -f ${gradleRepository})"
-eval "mkdir -p ${dataHome}"
 
 function dockerRm() {
     filter="$1"
@@ -29,13 +28,20 @@ function dockerLogsUntil() {
     endpoint="$2"
     containerId=$(docker ps -aq --filter "${filter}")
     nohup docker logs -f "${containerId}" > "/tmp/${containerId}.log" 2>&1 &
-    sleep 1s
     PID=$(ps aux | grep "docker" | grep ${containerId} | awk '{print $2}' | sort -nr | head -1)
     if [ "${PID}" != "" ]; then
         eval "tail -f --pid=${PID} /tmp/${containerId}.log | sed '/${endpoint}/q'"
-        kill -9 ${PID}
+        kill ${PID}
         rm /tmp/${containerId}.log
     fi
+}
+
+function rebuid() {
+    path=$1
+    if [ -e "${path}" ]; then
+        eval "rm -rf ${path}"
+    fi
+    eval "mkdir -p ${path}"
 }
 
 function dockerRun() {
@@ -58,13 +64,21 @@ function sedSource() {
 }
 
 dockerRm "ancestor=${imageTag}"
-dockerRun
-sedSource
-dockerRm "ancestor=${imageTag}"
+if [ "${cleanup}" = "true" ]; then
+    rebuid "${dataHome}"
+    dockerRun
+    dockerRm "ancestor=${imageTag}"
+    sedSource
+fi
 dockerRun
 
+# /usr/share/jenkins/jenkins.war
+
+# Jenkins initial setup is required. An admin user has been created and a password generated.
+# This may also be found at: /var/jenkins_home/secrets/initialAdminPassword
+
 # 已安装工具的路径和版本号
-# /opt/java/openjdk/bin/java jdk-8u262
+# /opt/java/openjdk/bin/java jdk-8u272
 # /usr/bin/git git-2.26.2
-# /app/gradle-6.6.1/bin/gradle
+# /app/gradle-6.7.1/bin/gradle
 # /app/apache-maven-3.6.3/bin/mvn
