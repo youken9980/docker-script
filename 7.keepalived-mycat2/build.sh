9f5a7d35a35c0ffd23e89819ffe7bf9490b4ce94
youken9980 <youken9980@163.com>
2023-03-01 14:09:42 +0800

    更新脚本

--- 7.keepalived-mycat2/build.sh
+++ 7.keepalived-mycat2/build.sh
@@ -3,5 +3,6 @@
 docker build \
     --add-host "ghproxy.com:141.147.152.25" \
     --add-host "raw.staticdn.net:104.21.31.232" \
+    --add-host "maven.aliyun.com:59.110.251.10" \
     -f Dockerfile \
     -t youken9980/keepalived-mycat2:latest .
