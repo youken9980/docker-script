b76b99a361eac273fa6d229bec62ecd248d52e5c
youken9980 <youken9980@163.com>
2023-09-21 11:15:47 +0800

    更新脚本

--- nginx/build.sh
+++ nginx/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker pull nginx:1.22-alpine
-docker build -f Dockerfile -t youken9980/nginx:1.22-alpine .
+docker pull nginx:1.24-alpine-slim
+docker build -f Dockerfile -t youken9980/nginx:1.24-alpine-slim .
