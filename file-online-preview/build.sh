743477d7e19c84a5857a0b994052938d9176ed2e
youken9980 <youken9980@163.com>
2023-12-22 11:42:15 +0800

    更新脚本

--- file-online-preview/build.sh
+++ file-online-preview/build.sh
@@ -3,5 +3,6 @@
 docker build \
     --add-host "releases.aspose.com:18.160.18.60" \
     --add-host "maven.aliyun.com:59.110.251.11" \
+    --add-host "repository.aspose.com:3.160.196.16" \
     -f Dockerfile \
     -t youken9980/file-online-preview:latest .
