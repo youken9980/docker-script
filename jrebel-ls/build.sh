f0e961ee07fd00b30b03e97debc990cbc56f62e3
youken9980 <youken9980@163.com>
2024-06-05 16:42:01 +0800

    更新脚本

--- jrebel-ls/build.sh
+++ jrebel-ls/build.sh
@@ -2,5 +2,6 @@
 
 docker build \
     --add-host "maven.aliyun.com:59.110.251.11" \
+    --build-arg "GITHUB_MIRROR=https://mirror.ghproxy.com/" \
     -f Dockerfile \
     -t youken9980/jrebel-ls:latest .
