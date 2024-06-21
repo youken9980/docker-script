0f0218d934bfded4b3d97e0249708a9bf55a6055
youken9980 <youken9980@163.com>
2024-06-21 23:01:37 +0800

    更新脚本

--- 2.build-tools/build.sh
+++ 2.build-tools/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-docker build -f Dockerfile -t youken9980/build-tools-temurin8:latest .
-docker tag youken9980/build-tools-temurin8:latest youken9980/build-tools-temurin8:repository
+docker build -f Dockerfile \
+    -t youken9980/build-tools-temurin8:latest \
+    -t youken9980/build-tools-temurin8:repository \
+    .
--- node/build.sh
+++ node/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build -f Dockerfile -t youken9980/node:14-alpine .
+docker build -f Dockerfile \
+    -t youken9980/node:14-alpine \
+    -t youken9980/node:14-alpine-repository \
+    .
