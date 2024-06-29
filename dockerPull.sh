ff7d39694b92d8b8835c09722ecb78e9f0c2d7fa
youken9980 <youken9980@163.com>
2024-06-29 22:59:43 +0800

    更新脚本

--- dockerPull.sh
+++ dockerPull.sh
@@ -5,8 +5,6 @@ docker pull alpine:3
 docker pull centos:7
 docker pull centos:8
 
-docker pull debian:bullseye
-docker pull debian:bullseye-slim
 docker pull debian:bookworm
 docker pull debian:bookworm-slim
 
@@ -14,20 +12,24 @@ docker pull ubuntu:jammy
 
 docker pull eclipse-temurin:8-jdk-alpine
 docker pull eclipse-temurin:8-jre-alpine
-docker pull eclipse-temurin:8-jdk-centos7
-docker pull eclipse-temurin:8-jre-centos7
-docker pull eclipse-temurin:8-jdk-jammy
 docker pull eclipse-temurin:8-jre-jammy
-docker pull eclipse-temurin:17-jre-jammy
+docker pull eclipse-temurin:21-jre-jammy
 
 docker pull golang:1.12-alpine
 
 docker pull jenkins/jenkins:lts-alpine
 docker pull jenkins/inbound-agent:alpine
 
-docker pull mysql:5.7-debian
+docker pull mysql:5-debian
 
-docker pull nginx:1.24-alpine-slim
+docker pull nginx:stable-alpine-slim
 
-docker pull tomcat:9-jdk8-temurin-jammy
+docker pull node:14-alpine
+
+docker pull osixia/keepalived:latest
+
+docker pull redis:7-alpine
+
+docker pull registry:2
+
 docker pull tomcat:9-jre8-temurin-jammy
